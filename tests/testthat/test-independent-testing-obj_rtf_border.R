b9219f173dd01ca4a6545c430a8f8f7ff9766db3
Yilong Zhang <yilong.zhang@merck.com>
2020-11-16 10:52:38 -0500

    add test for obj_rtf_border and obj_rtf_text

--- tests/testthat/test-independent-testing-obj_rtf_border.R
+++ tests/testthat/test-independent-testing-obj_rtf_border.R
@@ -1,15 +1,120 @@
-test_that("Check argument type, test if error message will show correctly", {
+#Build Temporary dataset for use in testing
+testdat <- iris[1:100,]
 
+test_that("Check argument type, test if error message will show correctly", {
+  expect_error(obj_rtf_border(tbl = testdat,border_left = 1),
+               "The argument type did not match: character")
+  expect_error(obj_rtf_border(tbl = testdat,border_right = 1),
+               "The argument type did not match: character")
+  expect_error(obj_rtf_border(tbl = testdat,border_top = 1),
+               "The argument type did not match: character")
+  expect_error(obj_rtf_border(tbl = testdat,border_bottom = 1),
+               "The argument type did not match: character")
+  expect_error(obj_rtf_border(tbl = testdat,border_color_left = 1),
+               "The argument type did not match: character")
+  expect_error(obj_rtf_border(tbl = testdat,border_color_right = 1),
+               "The argument type did not match: character")
+  expect_error(obj_rtf_border(tbl = testdat,border_color_top = 1),
+               "The argument type did not match: character")
+  expect_error(obj_rtf_border(tbl = testdat,border_color_bottom = 1),
+               "The argument type did not match: character")
+  expect_error(obj_rtf_border(tbl = testdat,cell_justification = 1),
+               "The argument type did not match: character")
+  expect_error(obj_rtf_border(tbl = testdat,border_width = "1"),
+               "The argument type did not match: integer/numeric")
+  expect_error(obj_rtf_border(tbl = testdat,cell_height = "1"),
+               "The argument type did not match: integer/numeric")
+  expect_error(obj_rtf_border(tbl = testdat,cell_nrow = "1"),
+               "The argument type did not match: integer/numeric")
 })
 
 test_that("Check argument values, test if error message will show correctly", {
-
+  expect_error(obj_rtf_border(tbl = testdat,border_left = "testing"))
+  expect_error(obj_rtf_border(tbl = testdat,border_right = "testing"))
+  expect_error(obj_rtf_border(tbl = testdat,border_top = "testing"))
+  expect_error(obj_rtf_border(tbl = testdat,border_bottom = "testing"))
+  expect_error(obj_rtf_border(tbl = testdat,border_color_left = "redd"))
+  expect_error(obj_rtf_border(tbl = testdat,border_color_right = 'redd'))
+  expect_error(obj_rtf_border(tbl = testdat,border_color_top = 'redd'))
+  expect_error(obj_rtf_border(tbl = testdat,border_color_bottom = "redd"))
+  expect_error(obj_rtf_border(tbl = testdat,cell_justification = "leftt"))
+  expect_error(obj_rtf_border(tbl = testdat,border_width = -1))
+  expect_error(obj_rtf_border(tbl = testdat,border_color_top = NULL,
+                              border_color_first = 'red' ))
+  expect_error(obj_rtf_border(tbl = testdat,border_color_bottom = NULL,
+                              border_color_last = 'red' ))
 })
 
 test_that("Test if attributes are assigned correctly", {
+  a <- obj_rtf_border(tbl = testdat,border_left = "single"
+                      ,border_right = "single"
+                      ,border_top = "single"
+                      ,border_bottom = "single"
+                      ,border_color_left = "red"
+                      ,border_color_right = "red"
+                      ,border_color_top = "red"
+                      ,border_color_bottom = "red"
+                      ,border_color_first = "red"
+                      ,border_color_last = "red"
+                      ,cell_justification = "l"
+                      ,border_width = 5
+                      ,cell_height = 5
+                      ,cell_nrow = 5
+  )
+  expect_equal(attr(a,"border_left")[1], "single")
+  expect_equal(attr(a,"border_right")[1], "single")
+  expect_equal(attr(a,"border_top")[1], "single")
+  expect_equal(attr(a,"border_bottom")[1], "single")
+  expect_equal(attr(a,"border_color_left")[1], "red")
+  expect_equal(attr(a,"border_color_right")[1], "red")
+  expect_equal(attr(a,"border_color_top")[1], "red")
+  expect_equal(attr(a,"border_color_bottom")[1], "red")
+  expect_equal(attr(a,"border_color_first")[1], "red")
+  expect_equal(attr(a,"border_color_last")[1], "red")
+  expect_equal(attr(a,"cell_justification")[1], "l")
+  expect_equal(attr(a,"border_width")[1], 5)
+  expect_equal(attr(a,"border_width")[1], 5)
+  expect_equal(attr(a,"border_width")[1], 5)
 
 })
 
 test_that("Test if color_used is derived correctly", {
+  a <- obj_rtf_border(tbl = testdat,border_left = "single"
+                      ,border_right = "single"
+                      ,border_top = "single"
+                      ,border_bottom = "single"
+                      ,border_color_left = "red"
+                      ,border_color_right = "red"
+                      ,border_color_top = "red"
+                      ,border_color_bottom = "red"
+                      ,border_color_first = "red"
+                      ,border_color_last = "red"
+                      ,cell_justification = "l"
+                      ,border_width = 5
+                      ,cell_height = 5
+                      ,cell_nrow = 5
+  )
+  expect_equal(attr(a,"use_color")[1], TRUE)
 
+  a <- obj_rtf_border(tbl = testdat,border_left = "single"
+                      ,border_right = "single"
+                      ,border_top = "single"
+                      ,border_bottom = "single"
+                      ,border_color_left = "black"
+                      ,border_color_right = "black"
+                      ,border_color_top = "black"
+                      ,border_color_bottom = "black"
+                      ,border_color_first = "black"
+                      ,border_color_last = "black"
+                      ,cell_justification = "l"
+                      ,border_width = 5
+                      ,cell_height = 5
+                      ,cell_nrow = 5
+  )
+  expect_equal(attr(a,"use_color")[1], FALSE)
+})
+
+test_that("Check transfer vector to matrix by row", {
+  expect_error(obj_rtf_border(tbl =  ,border_left = "testing")
+  )
 })
--- tests/testthat/test-independent-testing-obj_rtf_text.R
+++ tests/testthat/test-independent-testing-obj_rtf_text.R
@@ -1,15 +1,118 @@
 test_that("Check argument type, test if error message will show correctly", {
+  expect_error(obj_rtf_text(text = 1),
+               "The argument type did not match: character")
+  expect_error(obj_rtf_text(text="testing",text_font = "1"),
+               "The argument type did not match: integer/numeric")
+  expect_error(obj_rtf_text(text="testing",text_format = 1),
+               "The argument type did not match: character")
+  expect_error(obj_rtf_text(text="testing",text_font_size = "1"),
+               "The argument type did not match: integer/numeric")
+  expect_error(obj_rtf_text(text="testing",text_color = 1),
+               "The argument type did not match: character")
+  expect_error(obj_rtf_text(text="testing",text_background_color = 1),
+               "The argument type did not match: character")
+  expect_error(obj_rtf_text(text="testing",text_justification = 1),
+               "The argument type did not match: character")
+  expect_error(obj_rtf_text(text="testing",text_space_before = "1"),
+               "The argument type did not match: integer/numeric")
+  expect_error(obj_rtf_text(text="testing",text_space_after = "1"),
+               "The argument type did not match: integer/numeric")
+  expect_error(obj_rtf_text(text="testing",text_new_page = "1"),
+               "The argument type did not match: logical")
+  expect_error(obj_rtf_text(text="testing",text_hyphenation = "1"),
+               "The argument type did not match: logical")
+  expect_error(obj_rtf_text(text="testing",text_convert = "1"),
+               "The argument type did not match: logical")
 
 })
 
 test_that("Check argument values, test if error message will show correctly", {
-
+  expect_error(obj_rtf_text(text="testing",text_font = 20),)
+  expect_error(obj_rtf_text(text="testing",text_format = "aa"),)
+  expect_error(obj_rtf_text(text="testing",text_font_size = -1),)
+  expect_error(obj_rtf_text(text="testing",text_color = 'redd'),)
+  expect_error(obj_rtf_text(text="testing",text_background_color = "redd"),)
+  expect_error(obj_rtf_text(text="testing",text_justification = "left"),)
+  # expect_error(obj_rtf_text(text="testing",text_space_before = -1),)
+  # expect_error(obj_rtf_text(text="testing",text_space_after = -1),)
+  # expect_error(obj_rtf_text(text="testing",text_new_page = "1"),)
+  # expect_error(obj_rtf_text(text="testing",text_hyphenation = "1"),)
+  # expect_error(obj_rtf_text(text="testing",text_convert = "1"),)
+  #
 })
 
 test_that("Test if attributes are assigned correctly", {
+  a <- obj_rtf_text( text="testing"
+                     ,text_font = 1
+                     ,text_format = "b"
+                     ,text_font_size = 16
+                     ,text_color = "red"
+                     ,text_background_color = "red"
+                     ,text_justification = "l"
+                     ,text_indent_first = 0
+                     ,text_indent_left = 0
+                     ,text_indent_right = 0
+                     ,text_space = 1
+                     ,text_space_before = 15
+                     ,text_space_after = 15
+                     ,text_new_page = FALSE
+                     ,text_hyphenation = TRUE
+                     ,text_convert = TRUE
+  )
+  expect_equal(attr(a,"text_font")[1], 1)
+  expect_equal(attr(a,"text_format")[1], "b")
+  expect_equal(attr(a,"text_font_size")[1], 16)
+  expect_equal(attr(a,"text_color")[1], "red")
+  expect_equal(attr(a,"text_background_color")[1], "red")
+  expect_equal(attr(a,"text_justification")[1], "l")
+  expect_equal(attr(a,"text_indent_first")[1], 0)
+  expect_equal(attr(a,"text_indent_left")[1], 0)
+  expect_equal(attr(a,"text_indent_right")[1], 0)
+  expect_equal(attr(a,"text_space")[1], 1)
+  expect_equal(attr(a,"text_space_before")[1], 15)
+  expect_equal(attr(a,"text_space_after")[1], 15)
+  expect_equal(attr(a,"text_new_page")[1], FALSE)
+  expect_equal(attr(a,"text_hyphenation")[1], TRUE)
+  expect_equal(attr(a,"text_convert")[1], TRUE)
 
 })
 
 test_that("Test if color_used is derived correctly", {
+  a <- obj_rtf_text( text="testing"
+                     ,text_font = 1
+                     ,text_format = "b"
+                     ,text_font_size = 16
+                     ,text_color = "red"
+                     ,text_background_color = "red"
+                     ,text_justification = "l"
+                     ,text_indent_first = 0
+                     ,text_indent_left = 0
+                     ,text_indent_right = 0
+                     ,text_space = 1
+                     ,text_space_before = 15
+                     ,text_space_after = 15
+                     ,text_new_page = FALSE
+                     ,text_hyphenation = TRUE
+                     ,text_convert = TRUE
+  )
+  expect_equal(attr(a,"use_color")[1], TRUE)
 
+  a <- obj_rtf_text( text="testing"
+                     ,text_font = 1
+                     ,text_format = "b"
+                     ,text_font_size = 16
+                     ,text_color = "black"
+                     ,text_background_color = "black"
+                     ,text_justification = "l"
+                     ,text_indent_first = 0
+                     ,text_indent_left = 0
+                     ,text_indent_right = 0
+                     ,text_space = 1
+                     ,text_space_before = 15
+                     ,text_space_after = 15
+                     ,text_new_page = FALSE
+                     ,text_hyphenation = TRUE
+                     ,text_convert = TRUE
+  )
+  expect_equal(attr(a,"use_color")[1], FALSE)
 })
