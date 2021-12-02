7360fae9744c144a9d1a7f9a2ae93c5ee11044bc
Yilong Zhang <yilong.zhang@merck.com>
2021-12-01 19:51:46 -0500

    update test

--- tests/testthat/test-independent-testing-rtf_read.R
+++ tests/testthat/test-independent-testing-rtf_read.R
@@ -1,16 +1,12 @@
 test_that("read in PNG file in binary format", {
   df <- c("fig/fig1.png", "fig/fig2.png")
-
-  file_png_1 <- df[1]
-  file_png_2 <- df[2]
-
-  file_png_vec <- c(file_png_1, file_png_2)
-  png_vec_in <- rtf_read_figure(file_png_vec)
+  png_vec_in <- rtf_read_figure(df)
 
   png_in_1 <- readBin(file_png_1, what = "raw", size = 1, signed = TRUE, endian = "little", n = 1e8)
   png_in_2 <- readBin(file_png_2, what = "raw", size = 1, signed = TRUE, endian = "little", n = 1e8)
 
   png_lst <- list(png_in_1, png_in_2)
+  attr(png_vec_in, "fig_format") <- NULL
 
   expect_equal(png_vec_in, png_lst)
 })
