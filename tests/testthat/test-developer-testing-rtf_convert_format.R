0011490ff144e6ac093b2988c309c2a67ac41bcb
Yilong Zhang <yilong.zhang@merck.com>
2022-03-05 09:35:46 -0500

    skip test on CRAN

--- tests/testthat/test-developer-testing-rtf_convert_format.R
+++ tests/testthat/test-developer-testing-rtf_convert_format.R
@@ -1,15 +1,16 @@
-if (Sys.info()[["sysname"]] == "Linux") {
-  test_that("Report ERROR if libreoffice7.1 is not avaiable", {
-    file <- file.path(tempdir(), "tmp.rtf")
-    iris %>%
-      head() %>%
-      rtf_body() %>%
-      rtf_encode() %>%
-      write_rtf(file)
-    if (system("which libreoffice7.1", ignore.stderr = TRUE, ignore.stdout = TRUE) == 1) {
-      expect_error(rtf_convert_format(input = file, output_dir = tempdir()))
-    } else {
-      expect_equal(rtf_convert_format(input = file, output_dir = tempdir()), file.path(tempdir(), "tmp.pdf"))
-    }
-  })
-}
+test_that("Report ERROR if libreoffice7.1 is not avaiable", {
+  skip_on_cran()
+  skip_on_os(c("windows", "mac", "solaris"))
+  file <- file.path(tempdir(), "tmp.rtf")
+  iris %>%
+    head() %>%
+    rtf_body() %>%
+    rtf_encode() %>%
+    write_rtf(file)
+  if (system("which libreoffice7.1", ignore.stderr = TRUE, ignore.stdout = TRUE) == 1) {
+    expect_error(rtf_convert_format(input = file, output_dir = tempdir()))
+  } else {
+    expect_equal(rtf_convert_format(input = file, output_dir = tempdir()), file.path(tempdir(), "tmp.pdf"))
+  }
+})
+
